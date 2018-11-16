f075e9308595307ad675fe8587ee877bc03ba646
Olaf Hering <olaf@aepfle.de>
2018-11-16 17:29:48 +0100

    xen-4.10: new sysctl value
    Add 0x10 as sysctl value of xen-4.10
    
    bz#390553

--- include/vki/vki-xen-sysctl.h
+++ include/vki/vki-xen-sysctl.h
@@ -17,6 +17,7 @@
  * - 0x0000000d: Xen 4.7
  * - 0x0000000e: Xen 4.8
  * - 0x0000000f: Xen 4.9
+ * - 0x00000010: Xen 4.10
  *
  * When adding a new subop be sure to include the variants used by all
  * of the above, both here and in syswrap-xen.c
