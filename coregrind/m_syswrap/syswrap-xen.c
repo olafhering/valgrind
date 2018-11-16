1f0e84a7e77d019d8cc72f59b59f09d576d3bc84
Olaf Hering <olaf@aepfle.de>
2018-11-16 17:29:40 +0100

    xen-4.10: add domctl value
    Add 0xe as domctl value of xen-4.10
    
    bz#390553

--- coregrind/m_syswrap/syswrap-xen.c
+++ coregrind/m_syswrap/syswrap-xen.c
@@ -889,6 +889,9 @@ PRE(domctl)
    case 0x0000000d:
       vki_set_assumed_xenversion(vki_xenversion_409);
 	   break;
+   case 0x0000000e:
+      vki_set_assumed_xenversion(vki_xenversion_410);
+	   break;
    default:
       bad_intf_version(tid, layout, arrghs, status, flags,
                        "__HYPERVISOR_domctl", domctl->interface_version);
@@ -2194,6 +2197,7 @@ POST(domctl){
    case 0x0000000b:
    case 0x0000000c:
    case 0x0000000d:
+   case 0x0000000e:
 	   break;
    default:
 	   return;
