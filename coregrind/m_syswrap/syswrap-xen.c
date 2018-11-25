9e497b9c8bb7bf339178cdb0dbdc880e23109982
Olaf Hering <olaf@aepfle.de>
2018-11-25 01:33:58 +0100

    xen-4.10: handle all versioned sysctl ops
    When support for a new sysctl version is added, all versioned ops must be adjusted.
    
    Fixes commit c88133141a354d65568fb85037abc5e1f74ce46b.
    
    bz#390553

--- coregrind/m_syswrap/syswrap-xen.c
+++ coregrind/m_syswrap/syswrap-xen.c
@@ -793,6 +793,7 @@ PRE(sysctl) {
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          PRE_XEN_SYSCTL_READ(cputopoinfo_0000000c, num_cpus);
          PRE_XEN_SYSCTL_READ(cputopoinfo_0000000c, cputopo);
          break;
@@ -815,6 +816,7 @@ PRE(sysctl) {
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, num_nodes);
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, meminfo);
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, distance);
@@ -829,6 +831,7 @@ PRE(sysctl) {
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          PRE_XEN_SYSCTL_READ(pcitopoinfo_0000000c, num_devs);
          PRE_XEN_SYSCTL_READ(pcitopoinfo_0000000c, devs);
          break;
@@ -841,6 +844,7 @@ PRE(sysctl) {
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          PRE_XEN_SYSCTL_READ(cpu_featureset_0000000d, index);
          PRE_XEN_SYSCTL_READ(cpu_featureset_0000000d, nr_features);
          break;
@@ -2356,6 +2360,7 @@ POST(sysctl)
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          POST_XEN_SYSCTL_WRITE(cputopoinfo_0000000c, num_cpus);
          if (sysctl->u.cputopoinfo_0000000c.cputopo.p)
             POST_MEM_WRITE((Addr)sysctl->u.cputopoinfo_0000000c.cputopo.p,
@@ -2384,6 +2389,7 @@ POST(sysctl)
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          POST_XEN_SYSCTL_WRITE(numainfo_0000000c, num_nodes);
          POST_MEM_WRITE((Addr)sysctl->u.numainfo_0000000c.meminfo.p,
                         sizeof(uint64_t) * sysctl->u.numainfo_0000000c.num_nodes);
@@ -2401,6 +2407,7 @@ POST(sysctl)
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          POST_XEN_SYSCTL_WRITE(pcitopoinfo_0000000c, num_devs);
          POST_MEM_WRITE((Addr)sysctl->u.pcitopoinfo_0000000c.nodes.p,
                         sizeof(uint32_t) *  sysctl->u.pcitopoinfo_0000000c.num_devs);
@@ -2414,6 +2421,7 @@ POST(sysctl)
       case 0x0000000d:
       case 0x0000000e:
       case 0x0000000f:
+      case 0x00000010:
          POST_XEN_SYSCTL_WRITE(cpu_featureset_0000000d, nr_features);
          POST_MEM_WRITE((Addr)sysctl->u.cpu_featureset_0000000d.features.p,
                         sizeof(uint32_t) *  sysctl->u.cpu_featureset_0000000d.nr_features);
