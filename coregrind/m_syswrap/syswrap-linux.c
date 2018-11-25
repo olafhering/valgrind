74fe529f8b83d742b4b0ed860227257e66b0238d
Olaf Hering <olaf@aepfle.de>
2018-11-25 21:10:58 +0100

    xen-4.12: handle IOCTL_PRIVCMD_UNIMPLEMENTED

--- coregrind/m_syswrap/syswrap-linux.c
+++ coregrind/m_syswrap/syswrap-linux.c
@@ -8249,6 +8249,8 @@ PRE(sys_ioctl)
                   (Addr)&args->addr, sizeof(args->addr));
       break;
    }
+   case VKI_XEN_IOCTL_PRIVCMD_UNIMPLEMENTED:
+      break;
 
    case VKI_XEN_IOCTL_EVTCHN_BIND_VIRQ: {
          struct vki_xen_ioctl_evtchn_bind_virq *args =
@@ -10838,6 +10840,7 @@ POST(sys_ioctl)
       break;
    case VKI_XEN_IOCTL_PRIVCMD_RESTRICT:
    case VKI_XEN_IOCTL_PRIVCMD_MMAP_RESOURCE:
+   case VKI_XEN_IOCTL_PRIVCMD_UNIMPLEMENTED:
       break;
 
    case VKI_XEN_IOCTL_EVTCHN_BIND_VIRQ:
