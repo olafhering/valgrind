08dd0fefbbe678561ec8097032b61b6182ddfb77
Olaf Hering <olaf@aepfle.de>
2018-11-25 21:09:38 +0100

    xen-4.5: define domctl arm_configure_domain
    Add defines for arm_configure_domain domctl introduced in xen-4.5
    
    bz#390553

--- include/vki/vki-xen-domctl.h
+++ include/vki/vki-xen-domctl.h
@@ -125,6 +125,7 @@
 #define VKI_XEN_DOMCTL_set_vcpu_msrs                 73
 #define VKI_XEN_DOMCTL_setvnumainfo                  74
 #define VKI_XEN_DOMCTL_psr_cmt_op                    75
+#define VKI_XEN_DOMCTL_arm_configure_domain          76
 #define VKI_XEN_DOMCTL_monitor_op                    77 /* new in 4.6 */
 #define VKI_XEN_DOMCTL_gdbsx_guestmemio            1000
 #define VKI_XEN_DOMCTL_gdbsx_pausevcpu             1001
@@ -652,6 +653,7 @@ struct vki_xen_domctl {
         struct vki_xen_domctl_monitor_op_0000000b monitor_op_0000000b;
         //struct vki_xen_domctl_vnuma             vnuma;
         //struct vki_xen_domctl_psr_cmt_op        psr_cmt_op;
+        //struct vki_xen_domctl_arm_configuredomain configuredomain;
         vki_uint8_t                         pad[128];
     } u;
 };
