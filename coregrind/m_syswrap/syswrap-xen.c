33eebe2f329a042862300eed72fe9821e07b3f52
Olaf Hering <olaf@aepfle.de>
2018-11-25 21:10:29 +0100

    xen-4.9: handle all versioned sysctl ops
    When support for a new sysctl version is added, all versioned ops must be adjusted.
    
    bz#390553

--- coregrind/m_syswrap/syswrap-xen.c
+++ coregrind/m_syswrap/syswrap-xen.c
@@ -644,6 +644,7 @@ PRE(sysctl) {
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
 	 PRE_XEN_SYSCTL_READ(getdomaininfolist_0000000a, first_domain);
 	 PRE_XEN_SYSCTL_READ(getdomaininfolist_0000000a, max_domains);
 	 PRE_XEN_SYSCTL_READ(getdomaininfolist_0000000a, buffer);
@@ -714,6 +715,7 @@ PRE(sysctl) {
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          PRE_XEN_SYSCTL_READ(cputopoinfo_0000000c, num_cpus);
          PRE_XEN_SYSCTL_READ(cputopoinfo_0000000c, cputopo);
          break;
@@ -735,6 +737,7 @@ PRE(sysctl) {
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, num_nodes);
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, meminfo);
          PRE_XEN_SYSCTL_READ(numainfo_0000000c, distance);
@@ -748,6 +751,7 @@ PRE(sysctl) {
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          PRE_XEN_SYSCTL_READ(pcitopoinfo_0000000c, num_devs);
          PRE_XEN_SYSCTL_READ(pcitopoinfo_0000000c, devs);
          break;
@@ -759,6 +763,7 @@ PRE(sysctl) {
       {
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          PRE_XEN_SYSCTL_READ(cpu_featureset_0000000d, index);
          PRE_XEN_SYSCTL_READ(cpu_featureset_0000000d, nr_features);
          break;
@@ -1905,6 +1910,7 @@ POST(sysctl)
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
 	 POST_XEN_SYSCTL_WRITE(getdomaininfolist_0000000a, num_domains);
 	 POST_MEM_WRITE((Addr)sysctl->u.getdomaininfolist_0000000a.buffer.p,
 			sizeof(*sysctl->u.getdomaininfolist_0000000a.buffer.p)
@@ -1953,6 +1959,7 @@ POST(sysctl)
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          POST_XEN_SYSCTL_WRITE(physinfo_0000000a, threads_per_core);
          POST_XEN_SYSCTL_WRITE(physinfo_0000000a, cores_per_socket);
          POST_XEN_SYSCTL_WRITE(physinfo_0000000a, nr_cpus);
@@ -1992,6 +1999,7 @@ POST(sysctl)
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          POST_XEN_SYSCTL_WRITE(cputopoinfo_0000000c, num_cpus);
          if (sysctl->u.cputopoinfo_0000000c.cputopo.p)
             POST_MEM_WRITE((Addr)sysctl->u.cputopoinfo_0000000c.cputopo.p,
@@ -2019,6 +2027,7 @@ POST(sysctl)
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          POST_XEN_SYSCTL_WRITE(numainfo_0000000c, num_nodes);
          POST_MEM_WRITE((Addr)sysctl->u.numainfo_0000000c.meminfo.p,
                         sizeof(uint64_t) * sysctl->u.numainfo_0000000c.num_nodes);
@@ -2035,6 +2044,7 @@ POST(sysctl)
       case 0x0000000c:
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          POST_XEN_SYSCTL_WRITE(pcitopoinfo_0000000c, num_devs);
          POST_MEM_WRITE((Addr)sysctl->u.pcitopoinfo_0000000c.nodes.p,
                         sizeof(uint32_t) *  sysctl->u.pcitopoinfo_0000000c.num_devs);
@@ -2047,6 +2057,7 @@ POST(sysctl)
       {
       case 0x0000000d:
       case 0x0000000e:
+      case 0x0000000f:
          POST_XEN_SYSCTL_WRITE(cpu_featureset_0000000d, nr_features);
          POST_MEM_WRITE((Addr)sysctl->u.cpu_featureset_0000000d.features.p,
                         sizeof(uint32_t) *  sysctl->u.cpu_featureset_0000000d.nr_features);
