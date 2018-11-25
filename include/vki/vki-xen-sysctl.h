7cfd92de080f1daed08a67acc62ff616ed0d5cd3
Olaf Hering <olaf@aepfle.de>
2018-11-25 21:09:26 +0100

    xen-4.1: versioned XEN_SYSCTL_numainfo
    The XEN_SYSCTL_numainfo changed layout after xen-4.5.
    Rename layout and handle runtime from xen-4.1 upto xen-4.5.
    
    bz#390553

--- include/vki/vki-xen-sysctl.h
+++ include/vki/vki-xen-sysctl.h
@@ -124,7 +124,7 @@ struct vki_xen_sysctl_topologyinfo {
     VKI_XEN_GUEST_HANDLE_64(vki_uint32) cpu_to_node;
 };
 
-struct vki_xen_sysctl_numainfo {
+struct vki_xen_sysctl_numainfo_00000008 {
     vki_uint32_t max_node_index;
     VKI_XEN_GUEST_HANDLE_64(vki_uint64) node_to_memsize;
     VKI_XEN_GUEST_HANDLE_64(vki_uint64) node_to_memfree;
@@ -179,7 +179,7 @@ struct vki_xen_sysctl {
         struct vki_xen_sysctl_physinfo_00000008 physinfo_00000008;
         struct vki_xen_sysctl_physinfo_0000000a physinfo_0000000a;
         struct vki_xen_sysctl_topologyinfo      topologyinfo;
-        struct vki_xen_sysctl_numainfo          numainfo;
+        struct vki_xen_sysctl_numainfo_00000008 numainfo_00000008;
         struct vki_xen_sysctl_sched_id          sched_id;
         //struct vki_xen_sysctl_perfc_op          perfc_op;
         struct vki_xen_sysctl_getdomaininfolist_00000008 getdomaininfolist_00000008;
